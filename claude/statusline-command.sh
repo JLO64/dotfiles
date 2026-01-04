4db9b456dc2cf0948c189b64918d4ddbebfc7a07
Julian Lopez <64julianlopez@gmail.com>
2026-01-04 12:03:25 -0800

    Basic claude prompt with colors

--- claude/statusline-command.sh
+++ claude/statusline-command.sh
@@ -1,31 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 
 # This script generates the status line for Claude Code
 # Input: JSON with workspace info (piped via stdin)
 # Output: A single line of text that will be displayed in the status bar
 
 # Read the JSON input from Claude Code
-input=$(cat)
+##input=$(cat)
 
 # Extract the current working directory from the JSON
 # You can also access: .workspace.name, .workspace.git_branch, etc.
-cwd=$(echo "$input" | jq -r '.workspace.current_dir')
+##cwd=$(echo "$input" | jq -r '.workspace.current_dir')
 
 # Get the directory name (last part of the path)
 dir_name=$(basename "$cwd")
 
-# Get git branch if in a git repository
-git_branch=""
-if git -C "$cwd" rev-parse --git-dir &>/dev/null 2>&1; then
-    git_branch=$(git -C "$cwd" branch 2>/dev/null | grep '^*' | cut -d' ' -f2-)
-    if [[ -n $git_branch ]]; then
-        git_branch=" on  $git_branch"
-    fi
-fi
-
 # Build and output the status line
 # Tip: Use nerd font icons for a better look (https://www.nerdfonts.com/cheat-sheet)
-printf " %s%s" "$dir_name" "$git_branch"
+# printf " %s%s" "$dir_name" "$git_branch"
+
+claude_code_prompt="╭─\033[38;5;40m${OS_NAME} $(whoami)\033[0m \033[38;5;239min\033[0m \033[1m\033[38;5;226m${PWD/#$HOME/~}\033[0m\033[22m \033[38;5;239mat\033[0m 󰥔$(date +%H:%M:%S)"
+
+echo "$claude_code_prompt"
 
 # Examples of what you can add:
 # - Current time: $(date +%H:%M:%S)
